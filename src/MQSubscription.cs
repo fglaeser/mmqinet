6ea28aeccd33f6eb3a75d661bd507706a3153bfa
Facundo <fglaeser@gmail.com>
2018-12-04 16:19:33 -0300

    Add MQSubscription and basic functions.

---
+++ src/MQSubscription.cs
@@ -0,0 +1,86 @@
+﻿using Mmqi.Native;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Mmqi
+{
+  public class MQSubscription
+  {
+    protected IMQQueueManager qMgr;
+    internal int objectHandle;
+    internal int subscriptionHandle;
+    protected MQQueue _subQueue;
+
+    public bool IsOpen
+    {
+      get
+      {
+        return subscriptionHandle != MQC.MQHO_NONE && MQC.MQHO_UNUSABLE_HOBJ != subscriptionHandle;
+      }
+    }
+
+    public int SubQueueHandle { get { return objectHandle; } }
+
+    public int SubscriptionHandle { get { return subscriptionHandle; } }
+
+    public MQSubscription(IMQQueueManager queueMgr)
+    {
+      qMgr = queueMgr;
+    }
+
+    public void Subscribe(MQSubscriptionDescriptor subscriptionDescriptor, int subqueueHandle = MQC.MQHO_NONE)
+    {
+      int compCode, reason;
+      byte[] array = new byte[subscriptionDescriptor.GetRequiredBufferSize()];
+      subscriptionDescriptor.WriteStruct(array, 0);
+      objectHandle = subqueueHandle;
+      IntPtr intPtr = Marshal.AllocCoTaskMem(array.Length);
+      try
+      {
+        Marshal.Copy(array, 0, intPtr, array.Length);
+        Bindings.MQSUB(qMgr.Handle, intPtr, ref objectHandle, out subscriptionHandle, out compCode, out reason);
+      }
+      finally
+      {
+        if (intPtr != IntPtr.Zero) Marshal.FreeCoTaskMem(intPtr);
+      }
+      if (compCode != MQC.MQCC_OK) throw new MQException(compCode, reason);
+      _subQueue = new MQQueue(qMgr);
+      _subQueue.objectHandle = objectHandle;
+    }
+
+    public void Subscribe(string subscriptionName, int subscriptionOptions, string topicName, string topicObject = "", string alternateUserId = null, int subqueueHandle = MQC.MQHO_NONE)
+    {
+      var descriptor = new MQSubscriptionDescriptor();
+      if (!string.IsNullOrEmpty(alternateUserId))
+        descriptor.AlternateUserId = alternateUserId;
+
+      descriptor.ObjectName = topicObject;
+      descriptor.ObjectString.VSString = topicName;
+      descriptor.SubName.VSString = subscriptionName;
+      descriptor.Options = subscriptionOptions;
+      Subscribe(descriptor, subqueueHandle);
+    }
+
+    public void Close(int options = MQC.MQCO_NONE, bool closeSubQueue = true, int closeSubQueueOptions = MQC.MQCO_NONE)
+    {
+      int compCode, reason;
+      if (qMgr != null && qMgr.IsConnected && IsOpen)
+      {
+        Bindings.MQCLOSE(qMgr.Handle, ref subscriptionHandle, options, out compCode, out reason);
+        if (compCode != MQC.MQCC_OK) throw new MQException(compCode, reason);
+
+        subscriptionHandle = MQC.MQHO_UNUSABLE_HOBJ;
+
+        if (closeSubQueue)
+        {
+          _subQueue.CloseOptions = closeSubQueueOptions;
+          _subQueue.Close();
+        }
+      }
+    }
+
+    public void Get(MQMessage message, MQGetMessageOptions gmo) => _subQueue.Get(message, gmo);
+
+  }
+}
---
+++ src/Native/MQSD32.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Mmqi.Native
+{
+  public struct MQSD32
+  {
+    [MarshalAs(30, SizeConst = 4)]
+    private byte[] strucId;
+
+    private int version;
+
+    private int options;
+
+    [MarshalAs(30, SizeConst = 48)]
+    private byte[] objectName;
+
+    [MarshalAs(30, SizeConst = 12)]
+    private byte[] alternateUserId;
+
+    [MarshalAs(30, SizeConst = 40)]
+    private byte[] alternateSecurityId;
+
+    private int subExpiry;
+
+    private MQBase.structMQCHARV32 objectString;
+
+    private MQBase.structMQCHARV32 subName;
+
+    private MQBase.structMQCHARV32 subUserData;
+
+    [MarshalAs(30, SizeConst = 24)]
+    private byte[] subCorrelId;
+
+    private int pubPriority;
+
+    [MarshalAs(30, SizeConst = 32)]
+    private byte[] pubAccountingToken;
+
+    [MarshalAs(30, SizeConst = 32)]
+    private byte[] pubApplIdentityData;
+
+    private MQBase.structMQCHARV32 selectionString;
+
+    private int subLevel;
+
+    private MQBase.structMQCHARV32 resObjectString;
+
+    public byte[] StrucId
+    {
+      get
+      {
+        return this.strucId;
+      }
+      set
+      {
+        this.strucId = value;
+      }
+    }
+
+    public int Version
+    {
+      get
+      {
+        return this.version;
+      }
+      set
+      {
+        this.version = value;
+      }
+    }
+
+    public int Options
+    {
+      get
+      {
+        return this.options;
+      }
+      set
+      {
+        this.options = value;
+      }
+    }
+
+    public byte[] ObjectName
+    {
+      get
+      {
+        return this.objectName;
+      }
+      set
+      {
+        this.objectName = value;
+      }
+    }
+
+    public byte[] AlternateUserId
+    {
+      get
+      {
+        return this.alternateUserId;
+      }
+      set
+      {
+        this.alternateUserId = value;
+      }
+    }
+
+    public byte[] AlternateSecurityId
+    {
+      get
+      {
+        return this.alternateSecurityId;
+      }
+      set
+      {
+        this.alternateSecurityId = value;
+      }
+    }
+
+    public int SubExpiry
+    {
+      get
+      {
+        return this.subExpiry;
+      }
+      set
+      {
+        this.subExpiry = value;
+      }
+    }
+
+    public MQBase.structMQCHARV32 ObjectString
+    {
+      get
+      {
+        return this.objectString;
+      }
+      set
+      {
+        this.objectString = value;
+      }
+    }
+
+    public MQBase.structMQCHARV32 SubName
+    {
+      get
+      {
+        return this.subName;
+      }
+      set
+      {
+        this.subName = value;
+      }
+    }
+
+    public MQBase.structMQCHARV32 SubUserData
+    {
+      get
+      {
+        return this.subUserData;
+      }
+      set
+      {
+        this.subUserData = value;
+      }
+    }
+
+    public byte[] SubCorrelId
+    {
+      get
+      {
+        return this.subCorrelId;
+      }
+      set
+      {
+        this.subCorrelId = value;
+      }
+    }
+
+    public int PubPriority
+    {
+      get
+      {
+        return this.pubPriority;
+      }
+      set
+      {
+        this.pubPriority = value;
+      }
+    }
+
+    public byte[] PubAccountingToken
+    {
+      get
+      {
+        return this.pubAccountingToken;
+      }
+      set
+      {
+        this.pubAccountingToken = value;
+      }
+    }
+
+    public byte[] PubApplIdentityData
+    {
+      get
+      {
+        return this.pubApplIdentityData;
+      }
+      set
+      {
+        this.pubApplIdentityData = value;
+      }
+    }
+
+    public MQBase.structMQCHARV32 SelectionString
+    {
+      get
+      {
+        return this.selectionString;
+      }
+      set
+      {
+        this.selectionString = value;
+      }
+    }
+
+    public int SubLevel
+    {
+      get
+      {
+        return this.subLevel;
+      }
+      set
+      {
+        this.subLevel = value;
+      }
+    }
+
+    public MQBase.structMQCHARV32 ResObjectString
+    {
+      get
+      {
+        return this.resObjectString;
+      }
+      set
+      {
+        this.resObjectString = value;
+      }
+    }
+  }
+}
